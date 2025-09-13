27a0c93fcde6dc5c18e3ccc943c3f9b37a487ddd
Kulvir Singh <kulvirs734@gmail.com>
2025-09-14 01:39:54 +0530

    basic ahh checkout api

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,34 @@ serve({
 				});
 			},
 		},
+		"/api/v1/checkout": async (_req) => {
+			const session = await stripe.checkout.sessions.create({
+				mode: "payment",
+				line_items: [
+					{
+						price_data: {
+							currency: "inr",
+							unit_amount: 69 * 100, // (x * 100) cents = x dollars
+							product_data: {
+								name: "Kitty Sub",
+								description: "meow meow meow",
+							},
+						},
+						quantity: 1,
+					},
+				],
+				success_url: "http://localhost:42069",
+				cancel_url: "http://localhost:42069",
+			});
+
+			const redirectUrl = session.url;
+
+			if (!redirectUrl) {
+				throw new Error("Stripe checkout session url not present");
+			}
+
+			return Response.redirect(redirectUrl, 302);
+		},
 	},
 	port: 42069,
 	error(error) {
