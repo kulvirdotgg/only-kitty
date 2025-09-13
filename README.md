4abe271619c37e8da0b462de7fb3ab34b69e022d
Kulvir Singh <kulvirs734@gmail.com>
2025-09-13 23:11:38 +0530

    surely stripe has to come

---
+++ README.md
@@ -0,0 +1,16 @@
+# stripe payments handling (maybe)
+
+1. Run the seeding script
+```sh
+bun seed
+```
+
+2. Run the dev server
+```sh
+bun dev
+```
+
+3. Open browser
+```
+http://localhost:42069
+```
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
 import { serve } from "bun";
+import Stripe from "stripe";
+
+
+const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY!;
+
+const stripe = new Stripe(STRIPE_SECRET_KEY, {
+	apiVersion: "2025-08-27.basil",
+});
+
 serve({
 	routes: {
 		"/api/v1/meow": {
